e342fb0751a765954d0200b929f67d394e1f83ab
thibauttaglialatela <thibauttaglialatela@gmail.com>
2021-12-02 21:58:38 +0100

    modification tables Category et Program

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,9 +24,48 @@ class Category
      */
     private $name;
 
+    /**
+     * @param Program $program
+     * @return Category
+     */
+    public function addProgram(Program $program): self
+    {
+        if (!$this->programs->contains($program)) {
+            $this->programs[] = $program;
+            $program->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Program $program
+     * @return Category
+     */
+    public function removeProgram(Program $program): self
+    {
+        if ($this->programs->removeElement($program)) {
+            // set the owning side to null (unless already changed)
+            if ($program->getCategory() === $this) {
+                $program->setCategory(null);
+            }
+            return $this;
+        }
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @ORM/OneToMany(targetEntity=Program::class, mappedBy="category")
+     */
+    private $programs;
+
+    public function __construct()
+    {
+        $this->programs = new ArrayCollection();
     }
 
     public function getName(): ?string
@@ -39,4 +80,12 @@ class Category
         return $this;
     }
 
+
+    /**
+     * @return Collection|Program[]
+     */ 
+    public function getPrograms(): Collection
+    {
+        return $this->programs;
+    }
 }
