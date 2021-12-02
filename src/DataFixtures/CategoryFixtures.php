572ba08cb44b4f510cb652bc4418e5d3e0ef9d8d
thibauttaglialatela <thibauttaglialatela@gmail.com>
2021-12-02 15:32:55 +0100

    creation fichier CategoryFixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $category = new Category();
+        $category->setName('Horreur');
+        $manager->persist($category);
+        $manager->flush();
+    }
+
+}
+   
