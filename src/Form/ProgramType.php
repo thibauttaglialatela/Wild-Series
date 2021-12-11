177a8c51082834a9ac4a7991701a69d25149d7b7
thibauttaglialatela <thibauttaglialatela@gmail.com>
2021-12-11 10:59:11 +0100

    ajout du champ actor au formulaire

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Program;
+use App\Entity\Actor;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -21,6 +23,12 @@ class ProgramType extends AbstractType
             ->add('country', TextType::class)
             ->add('year', TextType::class)
             ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => false,
+            ])
         ;
     }
 
