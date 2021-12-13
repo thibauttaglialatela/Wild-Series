25aaa53cf9b14057fef7a742eafe7ec510e2319a
thibauttaglialatela <thibauttaglialatela@gmail.com>
2021-12-13 11:44:14 +0100

    MAJ method generate

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -6,13 +6,22 @@ use App\Entity\Program;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use App\Service\Slugify;
 
 class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
+
+    private $slugify;
+
+    public function __construct(Slugify $slugify)
+    {
+        $this->slugify = $slugify;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $program = new Program();
-        $program->setTitle('Game of Thrones');
+        $program->setTitle($this->slugify->generate($program->setTitle('Game of Thrones')));
         $program->setSynopsis('La lutte pour le trone de fer');
         $program->setCategory($this->getReference('category_3'));
         $program->setCountry('USA');
--- src/Service/Slugify.php
+++ src/Service/Slugify.php
@@ -7,7 +7,14 @@ class Slugify
 
     public function generate(string $input): string
     {
-        return str_replace(' ', '-', $input);
+        $characters = [' ', 'é', 'è', 'à', 'ç', 'ù'];
+        $replace = ['-', 'e', 'e', 'a', 'c', 'u'];
+        $output = str_replace($characters, $replace, $input);
+        $output = preg_replace("[^a-zA-Z]", "", $output);
+        $trimmed = trim($output);
+        $output = preg_replace('#[ -]+#', '-', $trimmed);
+        $output = strtolower($output);
+        return $output;
     }
 
 }
