5a8905eafdb21cccffd5678be6610b14d83d4639
thibauttaglialatela <thibauttaglialatela@gmail.com>
2021-12-28 16:13:39 +0100

    Création du lien de déconnection

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -25,4 +25,13 @@ class LoginController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout(): void
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
