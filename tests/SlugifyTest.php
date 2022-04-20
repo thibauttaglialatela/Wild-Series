b73a3575400b7ab8e9852f21fc927adc630e5535
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-04-20 15:25:17 +0200

    new version slugify

--- tests/SlugifyTest.php
+++ tests/SlugifyTest.php
@@ -10,10 +10,11 @@ class SlugifyTest extends TestCase
 {
     public function testGenerate(): void
     {
-        $slugify = new Slugify(); 
-        
+        $slugify = new Slugify();
+
         assertEquals('games-of-thrones', $slugify->generate('games of thrones'));
         assertEquals('anton-bank-linder', $slugify->generate(' anton - bank linder  '));
         assertEquals('metallica-hero-of-the-day', $slugify->generate('Metallica - Hero of the day'));
+        assertEquals('ce-jour-la-la-chute-de-shinganshina', $slugify->generate('Ce jour là, la chute de ShinganshinA'));
     }
 }
